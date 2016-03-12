f644d69657cae49bcaf50dbd45cb16d7c2b0af42
Michal Seidler <info@michalseidler.com>
2016-03-12 20:53:29 +0100

    MAC INIT

--- app/system/BasePresenter.php
+++ app/system/BasePresenter.php
@@ -2,13 +2,23 @@
 namespace App;
 
 use Nette;
+use Saja\AssetsLoader\Assets;
+use Saja\AssetsLoader\TAssetsLoader;
 
 class BasePresenter extends Nette\Application\UI\Presenter
 {
+    use TAssetsLoader;
+
     protected $db = null;
 
     public function __construct(Nette\Database\Context $database)
     {
+        parent::__construct();
         $this->db = $database;
+    }
+
+    public function startup(){
+        parent::startup();
+        $this->assetsLoader->load(Assets::BOOTSTRAP);
     }
 }
